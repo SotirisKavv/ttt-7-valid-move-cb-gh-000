4f7ca65fac3534699ce1bbd286e36450e4fc331f
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-08-23 19:30:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  return index>=0 && index<10
+  return position_taken?(board, index) && (index>=0 && index<10)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  if valid_move?(board, index)
     return board[index] == "X" || board[index] == "O"
-  else return nil end
 end
